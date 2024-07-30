2331a512bb0af03610700f3b74fb20772f9b1faf
Livio Dourado <liviodourado@gmail.com>
2024-07-30 15:38:32 -0300

    feat: Message Service

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,29 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Message} from '../model/message';
-import {tap} from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { MessagesService } from "./messages.service";
 
 @Component({
-  selector: 'messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  selector: "messages",
+  templateUrl: "./messages.component.html",
+  styleUrls: ["./messages.component.css"],
 })
 export class MessagesComponent implements OnInit {
+  showMessages = false;
 
+  errors$: Observable<string[]>;
 
-  constructor() {
-
+  constructor(public messagesService: MessagesService) {
+    console.log("Created messages component");
   }
 
   ngOnInit() {
+    console.log("messages component - OnInit");
 
-
+    this.errors$ = this.messagesService.errors$.pipe(
+      tap(() => (this.showMessages = true))
+    );
   }
-
 
   onClose() {
-
-
+    this.showMessages = false;
   }
-
 }
